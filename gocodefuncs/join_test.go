2b527068c554c8a33844fd670e91ae57b02ae5c6
Luby Ruffy <lubyruffy@gmail.com>
2022-10-20 14:41:09 +0800

    优化测试

--- gocodefuncs/join_test.go
+++ gocodefuncs/join_test.go
@@ -7,14 +7,14 @@ import (
 	"testing"
 )
 
-func TestJoinQuery(t *testing.T) {
+func joinQuery(c1, c2 string, t *testing.T) string {
 	fn1, err := utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"a":1}`)
+		_, err := f.WriteString(c1)
 		return err
 	})
 	assert.Nil(t, err)
 	fn2, err := utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"b":2}`)
+		_, err := f.WriteString(c2)
 		return err
 	})
 	assert.Nil(t, err)
@@ -26,50 +26,17 @@ func TestJoinQuery(t *testing.T) {
 	})
 	f, err := utils.ReadFirstLineOfFile(fr.OutFile)
 	assert.Nil(t, err)
-	assert.Equal(t, string(f), `{"a":"1","b":"2"}`)
+	return string(f)
+}
 
+func TestJoinQuery(t *testing.T) {
+
+	assert.Equal(t, joinQuery(`{"a":1}`, `{"b":2}`, t), `{"a":"1","b":"2"}`)
 	// 冲突
-	fn1, err = utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"a":1}`)
-		return err
-	})
-	assert.Nil(t, err)
-	fn2, err = utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"a":2}`)
-		return err
-	})
-	assert.Nil(t, err)
-	fr = Join(&testRunner{
-		T:        t,
-		lastFile: fn2,
-	}, map[string]interface{}{
-		"file": fn1,
-	})
-	f, err = utils.ReadFirstLineOfFile(fr.OutFile)
-	assert.Nil(t, err)
-	assert.Equal(t, string(f), `{"a":"2"}`)
-
+	assert.Equal(t, joinQuery(`{"a":1}`, `{"a":2}`, t), `{"a":"2"}`)
 	// 多行
-	fn1, err = utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"a":1}
-{"c":3}`)
-		return err
-	})
-	assert.Nil(t, err)
-	fn2, err = utils.WriteTempFile(".json", func(f *os.File) error {
-		_, err := f.WriteString(`{"b":2}
-{"d":4}`)
-		return err
-	})
-	assert.Nil(t, err)
-	fr = Join(&testRunner{
-		T:        t,
-		lastFile: fn2,
-	}, map[string]interface{}{
-		"file": fn1,
-	})
-	f, err = utils.ReadFirstLineOfFile(fr.OutFile)
-	assert.Nil(t, err)
-	assert.Equal(t, string(f), `{"a":"1","c":"3","b":"2","d":"4"}`)
+	assert.Equal(t, joinQuery(`{"a":1}
+{"c":3}`, `{"b":2}
+{"d":4}`, t), `{"a":"1","c":"3","b":"2","d":"4"}`)
 
 }
